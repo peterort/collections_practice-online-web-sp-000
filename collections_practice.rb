7d620a5718428e9aa66af82451385749801e20f3
Peter Ort <peter@peterort.com>
2020-05-04 18:40:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,4 +15,9 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
+  new_array = []
+  new_array << array[0]
+  new_array << array[1]
+  new_array << array[3]
+  new_array << array[2]
 end
