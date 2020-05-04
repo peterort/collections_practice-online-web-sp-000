96bf7bf8fe6c3380e337f34a2f5dbd200d844307
Peter Ort <peter@peterort.com>
2020-05-04 19:09:44 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -40,4 +40,11 @@ def sum_array(array)
 end
 
 def add_s(array)
+  array.each_with_index.collect do |word, index|
+    if index != 1
+      word = word + "s"
+    else
+      word = word
+    end
+  end
 end
